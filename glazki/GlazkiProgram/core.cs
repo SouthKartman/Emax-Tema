4bd672c5c62ccff9eebeb004c9d6f38a6690f0c5
Tema <93534577+SouthKartman@users.noreply.github.com>
2023-05-15 10:17:06 +0300

    Update

---
+++ glazki/GlazkiProgram/core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlazkiProgram
+{
+    internal class core
+    {
+        public static glazkisaveEntities db = new glazkisaveEntities();
+    }
+}
---
+++ glazki/GlazkiProgram/pages/StartPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GlazkiProgram.pages
+{
+    
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AgentBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new pages.AgentsPage());
+        }
+    }
+}
